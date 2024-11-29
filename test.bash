2dd2066bc19c0fb30adaf434818c1cccbda5fb32
Takanosuke Fukasawa <s23c1120zj@s.chibakoudai.jp>
2024-11-29 15:55:09 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
-	echo ${1}行目がちがよ
+	echo ${1}行目が違うよ
 	res=1
-}
+}	
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+### NOMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "{out}" = 15 ] || ng "$LINENO"
 
+### STRANGE INPUT ###
+out =$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
